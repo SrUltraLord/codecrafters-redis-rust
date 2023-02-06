4a6fcbe2033b4ef8d80041c70cf54c079a3bde0a
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-05 22:35:52 -0500

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::io::Result;
+use std::net::TcpListener;
 
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .expect("Could not bind to port 6379");
+
+    for stream in listener.incoming() {
+        match stream  {
+            Ok(_stream) => {
+                println!("Accepting connection!")
+            },
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
